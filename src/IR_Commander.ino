37bc0f30c30e598edbed8ab8b0cdeb7d10d63afa
https://github.com/eepromchip <eepromchip@gmail.com>
2018-11-27 16:37:52 -0500

    Updated to include Insignia Command

--- src/IR_Commander.ino
+++ src/IR_Commander.ino
@@ -5,7 +5,11 @@ IRsend irsend ;
 
 void setup( )
 {
+   //if ()
 irsend.sendNEC(0x20DF10EF, 32); // This command is for POWER for Vizio
+
+irsend.sendNEC(0x61A0F00F, 32); // This command is for POWER for Insignia
+
 }
 
 void loop( ) {
